cfd2b00a855359f378fea18a37fd14202f172460
Karen <karen.geerts@gmail.com>
2018-06-02 14:21:51 +1000

    add TodoListFooter

---
+++ src/app/todo-list-footer/todo-list-footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-footer',
+  templateUrl: './todo-list-footer.component.html',
+  styleUrls: ['./todo-list-footer.component.css']
+})
+export class TodoListFooterComponent implements OnInit {
+
+  @Input()
+  todos: Todo[];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
