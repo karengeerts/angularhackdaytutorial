9a368bbaae02ba6381aded4e82d7d75b8d6159e9
Karen <karen.geerts@gmail.com>
2018-06-02 14:29:35 +1000

    move TodoDataService Provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { TodoDataService} from './todo-data.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
-})
+  })
 export class AppComponent {
 
   //newTodo: Todo = new Todo();
